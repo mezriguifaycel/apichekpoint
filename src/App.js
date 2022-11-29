d3d593962595e769ae3681d3182125091da1e0ab
mezriguifaycel <mezrigui.faycel@gmail.com>
2022-11-29 16:48:57 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import '././App.css';
+import { Card, } from "react-bootstrap";
+import React from "react";
+import { Link } from "react-router-dom";
+function App({users}) {
+
+        return (
+
+                <div className="App1">
+                       
+                        <br/>
+                        {
+                        users.map( ( user, i ) => ( <Card className="App" key={ i } >
+
+                 <Card.Body>
+                    < Card.Title > { user.name } </Card.Title>
+                         < Card.Subtitle className="mb-2 text-muted" > { user.email } </Card.Subtitle>
+                         < Card.Text > { user.phone } </Card.Text>
+                                <Card.Link as={Link} to={`/${user.id}`}>Detaills</Card.Link>
+                                </Card.Body>
+                        </Card >
+                        ))
+                 }
+                </div>
+        );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {BrowserRouter as Router} from 'react-router-dom'
+import RouterComp from './Components/RouterComp';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+
+    <Router>
+    <RouterComp/>
+    </Router>
+ 
+);
